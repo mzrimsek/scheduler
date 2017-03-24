c5eda3d1e0ad42e1ef9179a3a2fed4f519afb51b
Mike Zrimsek <mikezrimsek@gmail.com>
2017-03-24 14:57:35 -0400

    add bash script argument count checking

--- build.sh
+++ build.sh
@@ -13,14 +13,18 @@ run() {
     dotnet run
 }
 
-OPTION=$1
-if [ "$OPTION" = "" ] || [ "$OPTION" = "build" ]; then
-	echo "Building..."
-	build
-elif [ "$OPTION" = "run" ]; then
-    echo "Building and running..."
-    run
+if [ "$#" -le 1 ]; then
+    OPTION=$1
+    if [ "$OPTION" = "" ] || [ "$OPTION" = "build" ]; then
+        echo "Building..."
+        build
+    elif [ "$OPTION" = "run" ]; then
+        echo "Building and running..."
+        run
+    else
+        echo "Invalid option"
+        echo "Valid options are 'build' (default) and 'run'"
+    fi
 else
-	echo "Invalid option"
-    echo "Valid options are 'build' (default) and 'run'"
+    echo "Too many arguments"
 fi
