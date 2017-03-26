741c1dbf9cbf1b690b0b7b9e89de524824b96fda
Brian Spray <Evenidontkno@live.com>
2017-03-26 15:55:26 -0400

    Guess I forgot some stuff
    Oops.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -23,6 +23,15 @@ $(function()
 	});
 });
 
- $(function () {
-    $('#datetimepicker1').datetimepicker();
+$(function () {
+    $('#datetimepicker6').datetimepicker();
+    $('#datetimepicker7').datetimepicker({
+        useCurrent: false //Important! See issue #1075
+    });
+    $("#datetimepicker6").on("dp.change", function (e) {
+        $('#datetimepicker7').data("DateTimePicker").minDate(e.date);
+    });
+    $("#datetimepicker7").on("dp.change", function (e) {
+        $('#datetimepicker6').data("DateTimePicker").maxDate(e.date);
+    });
 });
