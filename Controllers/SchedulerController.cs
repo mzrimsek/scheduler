c036314ef376e566775629e3d56d318b2605ad0b
Brian Spray <evenidontkno@live.com>
2017-03-29 22:08:07 -0400

    All sorts of changes

--- Controllers/SchedulerController.cs
+++ Controllers/SchedulerController.cs
@@ -1,12 +1,42 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using scheduler.Models;
 
 namespace scheduler.Controllers
 {
     public class SchedulerController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        private readonly ILogger _logger;
+
+        public SchedulerController(UserManager<ApplicationUser> userManager, ILoggerFactory loggerFactory) 
         {
-            return View();
+            _userManager = userManager;
+            _logger = loggerFactory.CreateLogger<SchedulerController>();
         }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+
+            var user = await GetCurrentUserAsync();
+
+            _logger.LogInformation(1, "Oh man all kinds of text just please work." + user);
+            
+            if (user != null) {
+                return View(); 
+            }
+            return RedirectToAction(nameof(AccountController.Login));          
+        }
+
+        private Task<ApplicationUser> GetCurrentUserAsync()
+        {
+            return _userManager.GetUserAsync(HttpContext.User);
+        }
+
     }
 }
