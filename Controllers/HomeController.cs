0ee2c7b89c691f0d672936874ff108e6df9bf7ca
Mike Zrimsek <mikezrimsek@gmail.com>
2017-04-05 18:26:05 -0400

    not wokring wtf is going on

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
+using scheduler.Data;
+using scheduler.Models.DatabaseModels;
+using scheduler.Repositories;
 
 namespace scheduler.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly EventRepository _eventRepo;
+
+        public HomeController()
+        {
+            var context = new ApplicationDbContext();
+            _eventRepo = new EventRepository(context);
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -19,6 +30,11 @@ namespace scheduler.Controllers
         public IActionResult Error()
         {
             return View();
+        }
+
+        public void Save(Event newEvent)
+        {
+            _eventRepo.Create(newEvent);
         }
     }
 }
--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -7,10 +7,7 @@ namespace scheduler.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
