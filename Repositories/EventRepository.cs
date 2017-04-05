c2051585d039db402f602cf01efcb1f118277117
Mike Zrimsek <mikezrimsek@gmail.com>
2017-04-05 16:46:55 -0400

    add more event stuff

--- Repositories/EventRepository.cs
+++ Repositories/EventRepository.cs
@@ -3,7 +3,8 @@ using System.Linq;
 using scheduler.Data;
 using scheduler.Models.DatabaseModels;
 
-namespace scheduler.Repositories {
+namespace scheduler.Repositories 
+{
     public class EventRepository
     {
         private readonly ApplicationDbContext _context;
@@ -21,6 +22,22 @@ namespace scheduler.Repositories {
         public List<Event> GetByCreatedId(int createdId) 
         {
             return _context.Events.Where(x => x.CreatedById == createdId).ToList();
+        }
+
+        public void Create(Event newEvent)
+        {
+            _context.Add(newEvent);
+            _context.SaveChanges();
+        }
+
+        public void Update(Event eventToUpdate)
+        {
+            var existingEvent = GetById(eventToUpdate.Id);
+            existingEvent.Title = eventToUpdate.Title;
+            existingEvent.Description = eventToUpdate.Description;
+            existingEvent.StartTime = eventToUpdate.StartTime;
+            existingEvent.EndTime = eventToUpdate.EndTime;
+            _context.SaveChanges();
         }
     }
 }
