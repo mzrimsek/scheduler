c2b1c92ed4938f85203481223e444396e3f3cb96
Mike Zrimsek <mikezrimsek@gmail.com>
2017-04-17 00:48:16 -0400

    using entire user object

--- Mappers/InviteeModelMapper.cs
+++ Mappers/InviteeModelMapper.cs
@@ -1,15 +1,16 @@
+using scheduler.Models;
 using scheduler.Models.DatabaseModels;
 
 namespace scheduler.Mappers
 {
     public static class InviteeModelMapper
     {
-        public static Invitee MapFrom(Event invitedEvent, string userId)
+        public static Invitee MapFrom(Event invitedEvent, ApplicationUser inivteeUser)
         {
             return new Invitee
             {
                 EventId = invitedEvent.Id,
-                UserId = userId,
+                UserId = inivteeUser.Id,
                 Accepted = false
             };
         }
