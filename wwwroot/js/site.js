cf91470adf49467d972a1f10f26e6bc7aa9e749c
Mike Zrimsek <mikezrimsek@gmail.com>
2017-05-02 04:54:09 -0400

    fixed the last broken styles for right  now

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,8 +1,8 @@
-var handleDateTimeField = function(id, inputType) {
+var handleDateTimeField = function (id, inputType) {
     let input = $("#" + id);
     let label = $("label[for='" + id + "']");
 
-    if(input.val()) {
+    if (input.val()) {
         document.getElementById(id).type = inputType;
     }
 
@@ -12,79 +12,79 @@ var handleDateTimeField = function(id, inputType) {
     });
 
     input.on("focusout", () => {
-        if(!input.val()) {
+        if (!input.val()) {
             document.getElementById(id).type = "text";
             label.show();
         }
     });
 }
 
-var initializeDateFields = function(startId, endId) {
+var initializeDateFields = function (startId, endId) {
     let startDate = $("#" + startId);
     let endDate = $("#" + endId);
 
     let startDateVal = startDate.val();
-    if(startDateVal) {
+    if (startDateVal) {
         endDate.attr("min", startDateVal);
     }
 
     let endDateVal = endDate.val();
-    if(endDateVal) {
+    if (endDateVal) {
         startDate.attr("max", endDateVal);
     }
 }
 
-var syncDateFields = function(startId, endId) {
+var syncDateFields = function (startId, endId) {
     let startDate = $("#" + startId);
     let endDate = $("#" + endId);
 
     startDate.on("change", () => {
         let startDateVal = startDate.val();
-        if(startDateVal) {
+        if (startDateVal) {
             endDate.attr("min", startDateVal);
         } else {
             endDate.attr("min", "");
         }
 
-        if(startDateVal > endDate.val()) {
+        if (startDateVal > endDate.val()) {
             endDate.val(startDateVal);
         }
     })
 
     endDate.on("change", () => {
         let endDateVal = endDate.val();
-        if(endDateVal) {
+        if (endDateVal) {
             startDate.attr("max", endDateVal);
         } else {
             startDate.attr("max", "");
         }
 
-        if(endDateVal < startDate.val()) {
+        if (endDateVal < startDate.val()) {
             startDate.val(endDateVal);
         }
     });
 }
 
-var syncTimeFields = function(startId, endId) {
+var syncTimeFields = function (startId, endId) {
     let startTime = $("#" + startId);
     let endTime = $("#" + endId);
 
     startTime.on("change", () => {
         let startTimeVal = startTime.val();
-        if(startTimeVal > endTime.val()) {
+        if (startTimeVal > endTime.val()) {
             endTime.val(startTimeVal);
         }
     })
 
     endTime.on("change", () => {
         let endTimeVal = endTime.val();
-        if(endTimeVal < startTime.val()) {
+        if (endTimeVal < startTime.val()) {
             startTime.val(endTimeVal);
         }
     });
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
     let startDateId = "startDate";
     let startTimeId = "startTime";
     let endDateId = "endDate";
@@ -99,7 +99,7 @@ $(document).ready(function() {
     handleDateTimeField(endTimeId, "time");
 
     let shouldRestrictTimes = $("#" + startDateId).val() == $("#" + endDateId).val();
-    if(shouldRestrictTimes) {
+    if (shouldRestrictTimes) {
         syncTimeFields(startTimeId, endTimeId);
     }
 });
